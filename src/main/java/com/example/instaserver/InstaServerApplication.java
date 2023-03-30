99a1a4d9e69a8338c0ace63b7d4076ae7dea207f
JEONG-JIWHAN <jjw981210@gmail.com>
2023-03-31 00:00:25 +0900

    feat: jayspt configure 작성

--- src/main/java/com/example/instaserver/InstaServerApplication.java
+++ src/main/java/com/example/instaserver/InstaServerApplication.java
@@ -1,11 +1,13 @@
 package com.example.instaserver;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableEncryptableProperties
 public class InstaServerApplication {
 
 	public static void main(String[] args) {

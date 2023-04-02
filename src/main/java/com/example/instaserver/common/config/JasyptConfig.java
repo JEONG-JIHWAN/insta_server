4dbcfbc56738802d4af47f2c5969c951b651c962
JEONG-JIWHAN <jjw981210@gmail.com>
2023-04-03 02:03:47 +0900

    feat: Jwt 생성 검증 기능 구현

--- src/main/java/com/example/instaserver/common/config/JasyptConfig.java
+++ src/main/java/com/example/instaserver/common/config/JasyptConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class JasyptConfigure {
+public class JasyptConfig {
     @Value("${jasypt.encryptor.password}")
     private String password;
 

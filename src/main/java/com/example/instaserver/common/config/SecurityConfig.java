8031e60b0f741112d2bfe5403af0c3fa1e72115d
JEONG-JIWHAN <jjw981210@gmail.com>
2023-04-03 02:05:25 +0900

    feat: Jwt 인증 필터 구현

--- src/main/java/com/example/instaserver/common/config/SecurityConfig.java
+++ src/main/java/com/example/instaserver/common/config/SecurityConfig.java
@@ -1,25 +1,48 @@
 package com.example.instaserver.common.config;
 
+import com.example.instaserver.auth.JwtAuthenticationFilter;
+import com.example.instaserver.auth.JwtConfig;
+import com.example.instaserver.auth.JwtProvider;
+import com.example.instaserver.user.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
     private static final String API_PREFIX = "/api";
+    private final JwtProvider jwtProvider;
+    private final JwtConfig jwtConfig;
+    private final UserService userService;
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter(jwtConfig.getHeader(), jwtProvider, userService);
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests()
-                .requestMatchers(API_PREFIX + "/sign-up").permitAll()
+                .requestMatchers(API_PREFIX + "/sign-up",
+                        API_PREFIX + "/login").permitAll()
                 .anyRequest().authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .formLogin().disable()
                 .httpBasic().disable()
                 .csrf().disable()
-                .rememberMe().disable();
+                .rememberMe().disable()
+                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }

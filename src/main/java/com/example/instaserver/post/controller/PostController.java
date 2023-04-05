d05fd0700efa34fb524f35f7d07611776696d000
JEONG-JIWHAN <jjw981210@gmail.com>
2023-04-05 12:29:39 +0900

    feat: 게시글 생성 기능 구현

---
+++ src/main/java/com/example/instaserver/post/controller/PostController.java
@@ -0,0 +1,25 @@
+package com.example.instaserver.post.controller;
+
+import com.example.instaserver.auth.CurrentUser;
+import com.example.instaserver.post.controller.dto.PostRequest;
+import com.example.instaserver.post.controller.dto.PostResponse;
+import com.example.instaserver.post.service.PostService;
+import com.example.instaserver.user.entity.User;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+@RequiredArgsConstructor
+public class PostController {
+    private final PostService postService;
+
+    @PostMapping(path = "post")
+    public PostResponse posting(@CurrentUser User user, @ModelAttribute PostRequest postRequest) throws IOException {
+        return postService.write(user, postRequest);
+    }
+}

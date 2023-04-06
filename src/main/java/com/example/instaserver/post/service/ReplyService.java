12e5b79752c919cef06575fe4f70c9a99ea7cbe8
JEONG-JIWHAN <jjw981210@gmail.com>
2023-04-06 20:00:22 +0900

    feat: 답글 삭제 기능 구현

--- src/main/java/com/example/instaserver/post/service/ReplyService.java
+++ src/main/java/com/example/instaserver/post/service/ReplyService.java
@@ -1,5 +1,7 @@
 package com.example.instaserver.post.service;
 
+import com.example.instaserver.common.exception.NotFoundException;
+import com.example.instaserver.post.controller.dto.reply.ReplyDeleteDto;
 import com.example.instaserver.post.controller.dto.reply.ReplyRequest;
 import com.example.instaserver.post.controller.dto.reply.ReplyResponse;
 import com.example.instaserver.post.entity.Comment;
@@ -9,6 +11,7 @@ import com.example.instaserver.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 @Service
 @Transactional(readOnly = true)
@@ -24,4 +27,19 @@ public class ReplyService {
         return ReplyResponse.from(newReply);
     }
 
+    @Transactional
+    public ReplyDeleteDto delete(User user, ReplyDeleteDto replyDeleteDto) {
+        Assert.notNull(user, "사용자가 존재하지 않습니다.");
+        Assert.notNull(replyDeleteDto.getId(), "답글 Id가 존재하지 않습니다.");
+        Reply reply = getReply(replyDeleteDto.getId());
+        Assert.isTrue(user.getId() == reply.getUser().getId(), "해당 답글을 삭제할 수 없습니다. ");
+
+        replyRepository.delete(reply);
+        return new ReplyDeleteDto(reply.getId());
+    }
+
+    public Reply getReply(Long replyId){
+        return replyRepository.findById(replyId)
+                .orElseThrow(() -> new NotFoundException("해당 답글이 존재하지 않습니다."));
+    }
 }
